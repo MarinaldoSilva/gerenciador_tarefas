985107d1b097dd6954d9536795e8b6b237c007f6
Mario <marinaldo12@hotmail.com>
2025-09-06 12:58:44 -0300

    arquivos e models iniciais do projeto

---
+++ tarefas/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+
+class Tarefas(models.Model):
+    titulo = models.CharField(max_length=150)
+    descricao = models.TextField()
+    concluido = models.BooleanField(default=False)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    dono = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.titulo}"

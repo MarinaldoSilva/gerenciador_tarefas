c3621701ba7c7b2e9eb3f63997fa548456559bf4
Mario <marinaldo12@hotmail.com>
2025-09-06 19:12:00 -0300

    arquivo de criação de secret_key para testar o projeto

--- create_secret_key.py
+++ create_secret_key.py
@@ -1,3 +1,4 @@
 from django.core.management.utils import get_random_secret_key
-
-print(get_random_secret_key())
+secret_key = get_random_secret_key()
+print(f"Sua chave para testar o meu projeto foi gerada: {secret_key}")
+print("Crie seu arquivo '.env' e crie sua variavel 'SECRET_KEY' e atribua e ela em aspas o valor da chave")

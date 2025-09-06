48f09e0baaeddb35223745be275d07f879ac041d
Mario <marinaldo12@hotmail.com>
2025-09-06 19:03:08 -0300

    inicio da utilização do django filters

--- tarefas/views.py
+++ tarefas/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from .serializers import Tarefaerializer
+from .models import Tarefa
 
-# Create your views here.
+class TarefaViewSet(viewsets.ModelViewSet):
+    serializer_class = Tarefaerializer
+    permission_classes = [permissions.IsAuthenticated]#obrigatórtio ser autenticado
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['concluido']
+    
+    def get_queryset(self):
+        return Tarefa.objects.filter(dono = self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(dono = self.request.user)
+
